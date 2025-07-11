51a9c380ed609710c4d060240888aefd8d480db8
Soin <147618435+Py-Jerry@users.noreply.github.com>
2025-07-11 18:42:13 +0800

    Update config.py
    修改配置文件

--- config.py
+++ config.py
@@ -6,8 +6,7 @@
 from loguru import logger
 from pymongo import MongoClient
 # MongoDB 连接
-CLIENT = MongoClient(
-    'mongodb://root:Aliyun_Mongo_20250218@dds-2vc3c96a7e797ee41197-pub.mongodb.cn-chengdu.rds.aliyuncs.com:3717,dds-2vc3c96a7e797ee42971-pub.mongodb.cn-chengdu.rds.aliyuncs.com:3717/admin?replicaSet=mgset-1150525521')
+CLIENT = MongoClient()
 DB = CLIENT["test_ytb"]
 # 日志
 logger.add("get_video_list.log", rotation="5 MB", encoding="utf-8", enqueue=True, retention="10 days")
